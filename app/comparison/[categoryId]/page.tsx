42560e81d85cbbe616e4cb929852f72683f39dba
Mahdi <mahdighajary@gmail.com>
2025-01-02 15:08:57 +0330

    Use base 2

--- app/comparison/[categoryId]/page.tsx
+++ app/comparison/[categoryId]/page.tsx
@@ -30,8 +30,8 @@ import PostVotingModal from '../ballot/modals/PostVotingModal';
 import NotFoundComponent from '@/app/components/404';
 import { NumberBox } from './NumberBox';
 
-const SliderMax = 3;
-const SliderBase = 10;
+const SliderMax = 10;
+const SliderBase = 2;
 
 const sliderScaleFunction = (x: number, base: number) => Math.floor(Math.pow(base, Math.abs(x)));
 
