15349021d278b07bb8f74b4875f4125521026461
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-01-21 22:32:25 +0530

    increase timeout

--- app/evaluation/SliderRationaleBox.tsx
+++ app/evaluation/SliderRationaleBox.tsx
@@ -172,7 +172,7 @@ export function SliderBox({
                   setIsLoading(true);
                   setTimeout(() => {
                     setIsLoading(false);
-                  }, 10000);
+                  }, 5000);
                   handleVote(editedRationale,
                     project1.id,
                     project2.id,
